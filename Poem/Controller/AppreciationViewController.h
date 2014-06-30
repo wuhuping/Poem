00867c5bb18d53effe8f2818798ed2294426bf4e
wuhuping <wuhuping@wuhupingtekiMac-mini.local>
2014-06-30 14:28:59 +0800

    add new file

---
+++ Poem/Controller/AppreciationViewController.h
@@ -0,0 +1,13 @@
+//
+//  AppreciationViewController.h
+//  Poem
+//
+//  Created by wuhuping on 14-6-30.
+//  Copyright (c) 2014年 wuhuping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppreciationViewController : UIViewController
+
+@end
---
+++ Poem/View/AuthorInfoView.h
@@ -0,0 +1,13 @@
+//
+//  AuthorInfoView.h
+//  Poem
+//
+//  Created by wuhuping on 14-6-30.
+//  Copyright (c) 2014年 wuhuping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AuthorInfoView : UIView
+
+@end
